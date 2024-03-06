32bce4088fa72537aa720b898c40466d1ec81ff9
NJKVARDA\trask <jirikvarda123@gmail.com>
2024-03-06 12:47:57 +0100

    Inicializuju si webdriver

--- src/main/java/com/example/eshopselenium/pageObjects/abstractComponents/AbstractComponents.java
+++ src/main/java/com/example/eshopselenium/pageObjects/abstractComponents/AbstractComponents.java
@@ -1,5 +1,14 @@
 package com.example.eshopselenium.pageObjects.abstractComponents;
 
+import org.openqa.selenium.WebDriver;
+import org.openqa.selenium.support.PageFactory;
+
 public class AbstractComponents {
 
+    WebDriver driver;
+
+    public AbstractComponents(WebDriver driver) {
+        this.driver = driver;
+        PageFactory.initElements(driver,this);
+    }
 }
