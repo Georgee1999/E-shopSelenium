bd4efb411487352b86b43b0d8f47a4ddd07deaa0
NJKVARDA\trask <jirikvarda123@gmail.com>
2024-03-06 13:20:59 +0100

    Create some elements on the Landing Page

--- src/main/java/com/example/eshopselenium/pageObjects/LandingPage.java
+++ src/main/java/com/example/eshopselenium/pageObjects/LandingPage.java
@@ -1,6 +1,45 @@
 package com.example.eshopselenium.pageObjects;
 
-public class LandingPage {
+import com.example.eshopselenium.pageObjects.abstractComponents.AbstractComponents;
+import org.openqa.selenium.WebDriver;
+import org.openqa.selenium.WebElement;
+import org.openqa.selenium.support.FindBy;
+import org.openqa.selenium.support.PageFactory;
+
+public class LandingPage extends AbstractComponents {
+
+    WebDriver driver;
+
+
+    public LandingPage(WebDriver driver) {
+        super(driver);
+        this.driver=driver;
+        PageFactory.initElements(driver,this);
+    }
+    public void goTo(){
+        driver.get("https://rahulshettyacademy.com/client");
+    }
+
+    // Elements
+    @FindBy(css = "h1[class='login-title']")
+    WebElement loginTitle;
+
+    @FindBy(css = "input[id='login']")
+    WebElement loginButton;
+
+    @FindBy(css = "a[class='btn1']")
+    WebElement registerButton;
+
+    @FindBy(id = "userEmail")
+    WebElement fieldForEmail;
+
+    @FindBy(id = "userPassword")
+    WebElement fieldForPassword;
+
+    @FindBy(css = "a[class='forgot-password-link']")
+    WebElement forgotPassword;
+
+
 
 
 }
---
+++ src/test/java/com/example/eshopselenium/testComponents/BaseTest.java
@@ -0,0 +1,2 @@
+package com.example.eshopselenium.testComponents;public class BaseTest {
+}
